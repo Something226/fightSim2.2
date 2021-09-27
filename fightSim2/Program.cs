db618ed2c570d8497c4a4c731f38bd30d55be162
Something226 <axel.lieback@gmail.com>
2021-09-27 12:35:20 +0200

    classes created

---
+++ fightSim2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fightSim2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fightSim2/Weapon.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace fightSim2
+{
+    public class Weapon
+    {
+        
+    }
+}
